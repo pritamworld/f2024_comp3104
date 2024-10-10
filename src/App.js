56b28b54dfc8b06dd9870be4224490f2b3a81442
moxDroid Labs Inc <pritam.world@gmail.com>
2024-10-09 22:00:29 -0400

    Set up GitHub Actions for deployment

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 10101010 || Pritesh Patel || DevOps</h5>
+      <h5>Developed By: 10101010 || Pritesh Patel || COMP 3104 || DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
